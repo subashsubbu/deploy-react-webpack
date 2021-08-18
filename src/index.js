3caab1128461af8ffeb8d0c1458f8978d602ee87
subashsubbu <35239091+subashsubbu@users.noreply.github.com>
2021-08-18 15:13:20 -0500

    test purpose

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {ExperimentWindow} from 'jspsych-react';
-import { timelineFactory } from './timeline';
-import callbackImageKeyboardResponsePlugin from './callbackImageKeyboardResponsePlugin';
+// import {ExperimentWindow} from 'jspsych-react';
+// import { timelineFactory } from './timeline';
+// import callbackImageKeyboardResponsePlugin from './callbackImageKeyboardResponsePlugin';
+import image from './static/000001.jpg'
 
 class App extends React.Component{
 	
     render(){
-	const callback = (targetID) => console.log(targetID);
-	const timeline = timelineFactory(callback);
+	// const callback = (targetID) => console.log(targetID);
+	// const timeline = timelineFactory(callback);
 
         return(
             <div>
-                <ExperimentWindow
+		<img src={image} width='100' height='100' />
+		<h1>Subash</h1>
+                {/* <ExperimentWindow
         		settings={{ timeline }}
         		plugins={{ callbackImageKeyboardResponsePlugin }}
-      		/>
+      		/> */}
+		
             </div>
         )
     }
